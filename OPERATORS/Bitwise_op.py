b19e1340125509f593b75fc353f470ec121e988d
Jagriti Yadav <jagritikumari0459@gmail.com>
2025-12-23 23:13:51 +0530

    Add Bitwise Operators demonstration in Python

---
+++ OPERATORS/Bitwise_op.py
@@ -0,0 +1,25 @@
+# Bitwise Operators in Python
+
+a = 5   # 0101
+b = 3   # 0011
+
+print("a =", a)
+print("b =", b)
+
+print("\n--- Bitwise AND ---")
+print("a & b =", a & b)
+
+print("\n--- Bitwise OR ---")
+print("a | b =", a | b)
+
+print("\n--- Bitwise XOR ---")
+print("a ^ b =", a ^ b)
+
+print("\n--- Bitwise NOT ---")
+print("~a =", ~a)
+
+print("\n--- Left Shift ---")
+print("a << 1 =", a << 1)
+
+print("\n--- Right Shift ---")
+print("a >> 1 =", a >> 1)
