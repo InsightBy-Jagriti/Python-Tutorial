c51773963f7f548972ce91551652799927deab69
Jagriti Yadav <jagritikumari0459@gmail.com>
2025-12-25 19:44:56 +0530

    Add basic if statement example in Python

---
+++ Conditional Statements/if.py
@@ -0,0 +1,11 @@
+# Basic example of the if statement in Python
+
+age = 20
+
+# The condition checks if age is 18 or more.
+# If True, the message below will be printed.
+if age >= 18:
+    print("You are an adult.")
+
+# If age is less than 18, this line is simply skipped.
+print("Program completed.")
